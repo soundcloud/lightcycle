d4330f5434130683dc4d2d0faf69d2fb1d5ae9da
Guillaume Lung <guillaume@soundcloud.com>
2016-04-18 12:06:53 +0200

    Remove useless `onCreate` overriden in `SampleActivity`.
    Fix #43

--- examples/basic/src/main/java/com/soundcloud/lightcycle/sample/basic/SampleActivity.java
+++ examples/basic/src/main/java/com/soundcloud/lightcycle/sample/basic/SampleActivity.java
@@ -3,15 +3,8 @@ package com.soundcloud.lightcycle.sample.basic;
 import com.soundcloud.lightcycle.LightCycle;
 import com.soundcloud.lightcycle.LightCycleAppCompatActivity;
 
-import android.os.Bundle;
-
 public class SampleActivity extends LightCycleAppCompatActivity<SampleActivity> {
     @LightCycle ActivityLogger activityLogger = new ActivityLogger();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     protected void setActivityContentView() {
