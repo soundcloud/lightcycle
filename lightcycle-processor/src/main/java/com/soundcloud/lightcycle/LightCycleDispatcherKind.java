64d63e23e5b0cc95d79ea5293b4531e94514fdb4
michaelengland <mg.england@gmail.com>
2016-09-28 14:36:16 +0200

    Add support preference compat fragment superclass
     - add preference v7 dep

--- lightcycle-processor/src/main/java/com/soundcloud/lightcycle/LightCycleDispatcherKind.java
+++ lightcycle-processor/src/main/java/com/soundcloud/lightcycle/LightCycleDispatcherKind.java
@@ -36,6 +36,17 @@ enum LightCycleDispatcherKind {
             return typeArgumentAsString("SupportFragmentLightCycle", dispatchedType);
         }
     },
+    DEFAULT_PREFERENCE_FRAGMENT {
+        @Override
+        boolean matches(Name name) {
+            return name.contentEquals("LightCyclePreferenceFragmentCompat");
+        }
+
+        @Override
+        String toTypeName(String dispatchedType) {
+            return typeArgumentAsString("SupportFragmentLightCycle", dispatchedType);
+        }
+    },
     BASE_ACTIVITY_DISPATCHER {
         @Override
         boolean matches(Name name) {
