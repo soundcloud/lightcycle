5ebcedb756d0153286ccba9675803e0628074850
Niko Yuwono <niko-yuwono@m3.com>
2016-11-04 16:56:25 +0900

    Remove created-by header

--- lightcycle-lib/src/main/java/com/soundcloud/lightcycle/util/Preconditions.java
+++ lightcycle-lib/src/main/java/com/soundcloud/lightcycle/util/Preconditions.java
@@ -4,10 +4,6 @@ import com.soundcloud.lightcycle.ActivityLightCycle;
 import com.soundcloud.lightcycle.FragmentLightCycle;
 import com.soundcloud.lightcycle.SupportFragmentLightCycle;
 
-/**
- * Created by yuwono-niko on 11/4/16.
- */
-
 public final class Preconditions {
 
     private static final String BINDING_TARGET_NULL_ERROR_MESSAGE = "Binding target must not be null";
