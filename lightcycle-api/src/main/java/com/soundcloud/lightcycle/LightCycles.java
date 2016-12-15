d038ece715fe89b89106a7db00b97391176bfcb8
Guillaume Lung <guillaume@soundcloud.com>
2016-12-15 16:34:45 +0100

    Lifted LightCycles of the same class are equals.
    Dispatchers rely on equals to deduce the list of attached light cycles. Lifted light cycles must implement it.
    Related to #36

--- lightcycle-api/src/main/java/com/soundcloud/lightcycle/LightCycles.java
+++ lightcycle-api/src/main/java/com/soundcloud/lightcycle/LightCycles.java
@@ -17,6 +17,8 @@ public final class LightCycles {
     private static final String ANDROID_PREFIX = "android.";
     private static final String JAVA_PREFIX = "java.";
 
+
+
     @SuppressWarnings("PMD.EmptyCatchBlock")
     public static void bind(LightCycleDispatcher<?> target) {
         Method bindingMethod;
@@ -54,204 +56,288 @@ public final class LightCycles {
     }
 
     public static <Source extends Activity, Target extends Source> ActivityLightCycle<Target> lift(final ActivityLightCycle<Source> lightCycle) {
-        return new ActivityLightCycle<Target>() {
-
-            @Override
-            public void onCreate(Target activity, Bundle bundle) {
-                lightCycle.onCreate(activity, bundle);
-            }
-
-            @Override
-            public void onNewIntent(Target activity, Intent intent) {
-                lightCycle.onNewIntent(activity, intent);
-            }
-
-            @Override
-            public void onStart(Target activity) {
-                lightCycle.onStart(activity);
-            }
-
-            @Override
-            public void onResume(Target activity) {
-                lightCycle.onResume(activity);
-            }
-
-            @Override
-            public boolean onOptionsItemSelected(Target activity, MenuItem item) {
-                return lightCycle.onOptionsItemSelected(activity, item);
-            }
-
-            @Override
-            public void onPause(Target activity) {
-                lightCycle.onPause(activity);
-            }
-
-            @Override
-            public void onStop(Target activity) {
-                lightCycle.onStop(activity);
-            }
-
-            @Override
-            public void onSaveInstanceState(Target activity, Bundle bundle) {
-                lightCycle.onSaveInstanceState(activity, bundle);
-            }
-
-            @Override
-            public void onRestoreInstanceState(Target activity, Bundle bundle) {
-                lightCycle.onRestoreInstanceState(activity, bundle);
-            }
-
-            @Override
-            public void onDestroy(Target activity) {
-                lightCycle.onDestroy(activity);
-            }
-        };
+        return new LiftedActivityLightCycle<>(lightCycle);
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static <Source extends android.app.Fragment, Target extends Source> FragmentLightCycle<Target> lift(final FragmentLightCycle<Source> lightCycle) {
-        return new FragmentLightCycle<Target>() {
-
-            @Override
-            public void onAttach(Target fragment, Activity activity) {
-                lightCycle.onAttach(fragment, activity);
-            }
-
-            @Override
-            public void onAttach(Target fragment, Context context) {
-                lightCycle.onAttach(fragment, context);
-            }
-
-            @Override
-            public void onCreate(Target fragment, Bundle bundle) {
-                lightCycle.onCreate(fragment, bundle);
-            }
-
-            @Override
-            public void onViewCreated(Target fragment, View view, Bundle savedInstanceState) {
-                lightCycle.onViewCreated(fragment, view, savedInstanceState);
-            }
-
-            @Override
-            public void onActivityCreated(Target fragment, Bundle bundle) {
-                lightCycle.onActivityCreated(fragment, bundle);
-            }
-
-            @Override
-            public void onStart(Target fragment) {
-                lightCycle.onStart(fragment);
-            }
-
-            @Override
-            public void onResume(Target fragment) {
-                lightCycle.onResume(fragment);
-            }
-
-            @Override
-            public boolean onOptionsItemSelected(Target fragment, MenuItem item) {
-                return lightCycle.onOptionsItemSelected(fragment, item);
-            }
-
-            @Override
-            public void onPause(Target fragment) {
-                lightCycle.onPause(fragment);
-            }
-
-            @Override
-            public void onStop(Target fragment) {
-                lightCycle.onStop(fragment);
-            }
-
-            @Override
-            public void onSaveInstanceState(Target fragment, Bundle bundle) {
-                lightCycle.onSaveInstanceState(fragment, bundle);
-            }
-
-            @Override
-            public void onDestroyView(Target fragment) {
-                lightCycle.onDestroyView(fragment);
-            }
-
-            @Override
-            public void onDestroy(Target fragment) {
-                lightCycle.onDestroy(fragment);
-            }
-
-            @Override
-            public void onDetach(Target fragment) {
-                lightCycle.onDetach(fragment);
-            }
-        };
+        return new LiftedFragmentLightCycle<>(lightCycle);
     }
 
 
     public static <Source extends android.support.v4.app.Fragment, Target extends Source> SupportFragmentLightCycle<Target> lift(final SupportFragmentLightCycle<Source> lightCycle) {
-        return new SupportFragmentLightCycle<Target>() {
+        return new LiftedSupportFragmentLightCycle<>(lightCycle);
+    }
 
-            @Override
-            public void onAttach(Target fragment, Activity activity) {
-                lightCycle.onAttach(fragment, activity);
+    static final class LiftedActivityLightCycle<Source extends Activity, Target extends Source> implements ActivityLightCycle<Target> {
+
+        private final ActivityLightCycle<Source> lightCycle;
+
+        LiftedActivityLightCycle(ActivityLightCycle<Source> lightCycle) {
+            this.lightCycle = lightCycle;
+        }
+
+        @Override
+        public void onCreate(Target activity, Bundle bundle) {
+            lightCycle.onCreate(activity, bundle);
+        }
+
+        @Override
+        public void onNewIntent(Target activity, Intent intent) {
+            lightCycle.onNewIntent(activity, intent);
+        }
+
+        @Override
+        public void onStart(Target activity) {
+            lightCycle.onStart(activity);
+        }
+
+        @Override
+        public void onResume(Target activity) {
+            lightCycle.onResume(activity);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(Target activity, MenuItem item) {
+            return lightCycle.onOptionsItemSelected(activity, item);
+        }
+
+        @Override
+        public void onPause(Target activity) {
+            lightCycle.onPause(activity);
+        }
+
+        @Override
+        public void onStop(Target activity) {
+            lightCycle.onStop(activity);
+        }
+
+        @Override
+        public void onSaveInstanceState(Target activity, Bundle bundle) {
+            lightCycle.onSaveInstanceState(activity, bundle);
+        }
+
+        @Override
+        public void onRestoreInstanceState(Target activity, Bundle bundle) {
+            lightCycle.onRestoreInstanceState(activity, bundle);
+        }
+
+        @Override
+        public void onDestroy(Target activity) {
+            lightCycle.onDestroy(activity);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
             }
 
-            @Override
-            public void onCreate(Target fragment, Bundle bundle) {
-                lightCycle.onCreate(fragment, bundle);
+            LiftedActivityLightCycle<?, ?> that = (LiftedActivityLightCycle<?, ?>) o;
+
+            return lightCycle != null ? lightCycle.equals(that.lightCycle) : that.lightCycle == null;
+        }
+
+        @Override
+        public int hashCode() {
+            return lightCycle != null ? lightCycle.hashCode() : 0;
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    static final class LiftedFragmentLightCycle<Source extends android.app.Fragment, Target extends Source> implements FragmentLightCycle<Target> {
+
+        private final FragmentLightCycle<Source> lightCycle;
+
+        LiftedFragmentLightCycle(FragmentLightCycle<Source> lightCycle) {
+            this.lightCycle = lightCycle;
+        }
+
+        @Override
+        public void onAttach(Target fragment, Activity activity) {
+            lightCycle.onAttach(fragment, activity);
+        }
+
+        @Override
+        public void onAttach(Target fragment, Context context) {
+            lightCycle.onAttach(fragment, context);
+        }
+
+        @Override
+        public void onCreate(Target fragment, Bundle bundle) {
+            lightCycle.onCreate(fragment, bundle);
+        }
+
+        @Override
+        public void onViewCreated(Target fragment, View view, Bundle savedInstanceState) {
+            lightCycle.onViewCreated(fragment, view, savedInstanceState);
+        }
+
+        @Override
+        public void onActivityCreated(Target fragment, Bundle bundle) {
+            lightCycle.onActivityCreated(fragment, bundle);
+        }
+
+        @Override
+        public void onStart(Target fragment) {
+            lightCycle.onStart(fragment);
+        }
+
+        @Override
+        public void onResume(Target fragment) {
+            lightCycle.onResume(fragment);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(Target fragment, MenuItem item) {
+            return lightCycle.onOptionsItemSelected(fragment, item);
+        }
+
+        @Override
+        public void onPause(Target fragment) {
+            lightCycle.onPause(fragment);
+        }
+
+        @Override
+        public void onStop(Target fragment) {
+            lightCycle.onStop(fragment);
+        }
+
+        @Override
+        public void onSaveInstanceState(Target fragment, Bundle bundle) {
+            lightCycle.onSaveInstanceState(fragment, bundle);
+        }
+
+        @Override
+        public void onDestroyView(Target fragment) {
+            lightCycle.onDestroyView(fragment);
+        }
+
+        @Override
+        public void onDestroy(Target fragment) {
+            lightCycle.onDestroy(fragment);
+        }
+
+        @Override
+        public void onDetach(Target fragment) {
+            lightCycle.onDetach(fragment);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
             }
 
-            @Override
-            public void onViewCreated(Target fragment, View view, Bundle savedInstanceState) {
-                lightCycle.onViewCreated(fragment, view, savedInstanceState);
+            LiftedFragmentLightCycle<?, ?> that = (LiftedFragmentLightCycle<?, ?>) o;
+
+            return lightCycle != null ? lightCycle.equals(that.lightCycle) : that.lightCycle == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            return lightCycle != null ? lightCycle.hashCode() : 0;
+        }
+    }
+
+    static final class LiftedSupportFragmentLightCycle<Source extends android.support.v4.app.Fragment, Target extends Source> implements SupportFragmentLightCycle<Target> {
+
+        private final SupportFragmentLightCycle<Source> lightCycle;
+
+        LiftedSupportFragmentLightCycle(SupportFragmentLightCycle<Source> lightCycle) {
+            this.lightCycle = lightCycle;
+        }
+
+        @Override
+        public void onAttach(Target fragment, Activity activity) {
+            lightCycle.onAttach(fragment, activity);
+        }
+
+        @Override
+        public void onCreate(Target fragment, Bundle bundle) {
+            lightCycle.onCreate(fragment, bundle);
+        }
+
+        @Override
+        public void onViewCreated(Target fragment, View view, Bundle savedInstanceState) {
+            lightCycle.onViewCreated(fragment, view, savedInstanceState);
+        }
+
+        @Override
+        public void onActivityCreated(Target fragment, Bundle bundle) {
+            lightCycle.onActivityCreated(fragment, bundle);
+        }
+
+        @Override
+        public void onStart(Target fragment) {
+            lightCycle.onStart(fragment);
+        }
+
+        @Override
+        public void onResume(Target fragment) {
+            lightCycle.onResume(fragment);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(Target fragment, MenuItem item) {
+            return lightCycle.onOptionsItemSelected(fragment, item);
+        }
+
+        @Override
+        public void onPause(Target fragment) {
+            lightCycle.onPause(fragment);
+        }
+
+        @Override
+        public void onStop(Target fragment) {
+            lightCycle.onStop(fragment);
+        }
+
+        @Override
+        public void onSaveInstanceState(Target fragment, Bundle bundle) {
+            lightCycle.onSaveInstanceState(fragment, bundle);
+        }
+
+        @Override
+        public void onDestroyView(Target fragment) {
+            lightCycle.onDestroyView(fragment);
+        }
+
+        @Override
+        public void onDestroy(Target fragment) {
+            lightCycle.onDestroy(fragment);
+        }
+
+        @Override
+        public void onDetach(Target fragment) {
+            lightCycle.onDetach(fragment);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
             }
 
-            @Override
-            public void onActivityCreated(Target fragment, Bundle bundle) {
-                lightCycle.onActivityCreated(fragment, bundle);
-            }
+            LiftedSupportFragmentLightCycle<?, ?> that = (LiftedSupportFragmentLightCycle<?, ?>) o;
 
-            @Override
-            public void onStart(Target fragment) {
-                lightCycle.onStart(fragment);
-            }
+            return lightCycle != null ? lightCycle.equals(that.lightCycle) : that.lightCycle == null;
 
-            @Override
-            public void onResume(Target fragment) {
-                lightCycle.onResume(fragment);
-            }
+        }
 
-            @Override
-            public boolean onOptionsItemSelected(Target fragment, MenuItem item) {
-                return lightCycle.onOptionsItemSelected(fragment, item);
-            }
-
-            @Override
-            public void onPause(Target fragment) {
-                lightCycle.onPause(fragment);
-            }
-
-            @Override
-            public void onStop(Target fragment) {
-                lightCycle.onStop(fragment);
-            }
-
-            @Override
-            public void onSaveInstanceState(Target fragment, Bundle bundle) {
-                lightCycle.onSaveInstanceState(fragment, bundle);
-            }
-
-            @Override
-            public void onDestroyView(Target fragment) {
-                lightCycle.onDestroyView(fragment);
-            }
-
-            @Override
-            public void onDestroy(Target fragment) {
-                lightCycle.onDestroy(fragment);
-            }
-
-            @Override
-            public void onDetach(Target fragment) {
-                lightCycle.onDetach(fragment);
-            }
-        };
+        @Override
+        public int hashCode() {
+            return lightCycle != null ? lightCycle.hashCode() : 0;
+        }
     }
 }
