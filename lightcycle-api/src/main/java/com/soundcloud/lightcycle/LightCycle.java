175620e04cb5e9a32ae241e218bd6f91897a7fb9
Riccardo Ciovati <rciovati@gmail.com>
2018-07-17 10:50:59 +0200

    Support Gradle incremental annotation processing (#101)

--- lightcycle-api/src/main/java/com/soundcloud/lightcycle/LightCycle.java
+++ lightcycle-api/src/main/java/com/soundcloud/lightcycle/LightCycle.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.CLASS)
 @Target(ElementType.FIELD)
 public @interface LightCycle {
 }
